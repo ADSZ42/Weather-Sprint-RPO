4fe5afa84ca7a4f7d794efbbc4f05816c05e6e2f
ADSZ42 <nnancrs1@gmail.com>
2024-12-11 16:05:22 -0800

    wed change

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,13 @@
 import { APIKEY } from './environment.js';   
 
+let cityName = document.getElementById("cityName");
+let currTempD1 = document.getElementById("currTempD1");
+let hiTempD1 = document.getElementById("hiTempD1");
+let loTempD1 = document.getElementById("loTempD1");
+let windD1 = document.getElementById("windD1");
+let percRainD1 = document.getElementById("percRainD1");
+
+
 navigator.geolocation.getCurrentPosition(success, errorFunc);
 
 {
